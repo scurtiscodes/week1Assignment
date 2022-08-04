9b9103facfb964a68bb2f2007c356cce90cc48d9
Samuel Curtis <samuelcurtis@Caitlins-iMac.home>
2022-08-03 23:01:11 -0400

    index.js and index.html commited

---
+++ index.js
@@ -0,0 +1,21 @@
+let bankAccountBalance = 500;
+let numberOfFriends = 4;
+
+console.log('Hello World');
+
+console.log(5 + 3);
+console.log(4 - 2);
+console.log(6 * 7);
+console.log(8 / 2);
+
+console.log(bankAccountBalance - 30);
+console.log(bankAccountBalance * numberOfFriends);
+
+console.log(5 == 5);
+console.log(5 == 6);
+console.log(bankAccountBalance == numberOfFriends);
+console.log(bankAccountBalance == 500);
+let name = 'Sam';
+console.log(name);
+console.log(bankAccountBalance -= 6);
+console.log(bankAccountBalance += 5);
