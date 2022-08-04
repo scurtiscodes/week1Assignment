f9f0d2b230dadee34a8cf5ea46930c01810b0522
Samuel Curtis <samuelcurtis@Caitlins-iMac.home>
2022-08-03 23:53:06 -0400

    added code lines 23-33

--- index.js
+++ index.js
@@ -19,3 +19,15 @@ let name = 'Sam';
 console.log(name);
 console.log(bankAccountBalance -= 6);
 console.log(bankAccountBalance += 5);
+
+var firstName = "Samuel";
+var lastName = "Curtis";
+var age = 32;
+var numberOfCats = 3;
+var hasPets = true;
+    if (hasPets = true) {hasPets = " has "}
+        else {hasPets = " does not have "}
+var fullName = firstName + " " + lastName;
+
+console.log(fullName);
+console.log(fullName + hasPets + "pets. ");
